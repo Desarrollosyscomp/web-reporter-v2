b5ac54ff2b036a69a5e62c0942e87c70f148f862
carlos-casas <carloscasasramirez@gmail.com>
2026-03-02 10:35:23 -0500

    minor bugs fixed

--- src/types/request-with-tenant.ts
+++ src/types/request-with-tenant.ts
@@ -1,13 +1,13 @@
 import { Request } from 'express';
 
-export type Ttenant = {
-  database: string;
-  ip: string;
-};
-
 export type TTenantMySQLConfig = {
     host: string;
     database: string;
+}
+
+export type Ttenant = {
+    database: string;
+    ip: string;
 }
 
 export interface RequestWithTenant extends Request {
