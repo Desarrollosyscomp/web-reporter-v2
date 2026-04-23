ead5dda0506a4d1ff2fdfe20a7aaad981f6bce0e
Fernanda Astaiza <yulieth.astaiza@soy.sena.edu.co>
2026-04-23 10:49:55 -0500

    log added

--- src/database/mysql/mysql.connection.ts
+++ src/database/mysql/mysql.connection.ts
@@ -17,7 +17,7 @@ export class MySQLConnectionFactory {
         connectionLimit: databaseConfig.connectionLimit,
         waitForConnections: true,
       });
-
+      console.log(pool);
       this.pools.set(key, pool);
     }
     return this.pools.get(key)!;
